c3a4c90149e2d4de337e5f57694f065db1a9a213
cartel360 <billycartel360@gmail.com>
2023-04-25 21:59:11 +0300

    Add: Improvements

--- src/app/pages/all-courses/all-courses.component.ts
+++ src/app/pages/all-courses/all-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-all-courses',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-courses.component.css']
 })
 export class AllCoursesComponent {
+  courses: any
 
+  constructor (private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.Get('courses').subscribe((res: any) => {
+      this.courses = res;
+    });
+  }
 }
--- src/app/pages/dashboard/sidenav/resources/resources.component.ts
+++ src/app/pages/dashboard/sidenav/resources/resources.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-resources',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./resources.component.css']
 })
 export class ResourcesComponent {
+  courses: any;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.Get('courses').subscribe((res: any) => {
+      this.courses = res;
+    });
+  }
 }
