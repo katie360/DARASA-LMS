f0f8c27b53efe86b5e72ed5b44f5069866d67cec
cartel360 <billycartel360@gmail.com>
2023-05-10 14:01:29 +0300

    #Fix: Signup now works

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -17,8 +17,16 @@ export class SignUpComponent {
   signUp() {
     const body = { email: this.email, password1: this.password1, password2: this.password2 };
     this.apiService.Post('auth/register/', body).subscribe((res: any) => {
-      localStorage.setItem('token', res.token);
-      localStorage.setItem('user', JSON.stringify(res.user));
+      // clear form
+      this.name = '';
+      this.email = '';
+      this.password1 = '';
+      this.password2 = '';
+      
+      alert('User created successfully!, Please login');
+
+      // redirect to login page
+      window.location.href = '/login';
 
     }, (error: any) => {
       alert({ error});
